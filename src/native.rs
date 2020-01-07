6cb50e986b5247a83012f08d4a45f5b61ef723c2
Chris Pearce <christopher.james.pearce@gmail.com>
2020-01-07 11:26:20 +0000

    Correct typo in wasm feature flag

--- src/native.rs
+++ src/native.rs
@@ -3,5 +3,5 @@
 #[cfg(all(feature = "curl_client", not(target_arch = "wasm32")))]
 pub use super::isahc::IsahcClient as NativeClient;
 
-#[cfg(all(feature = "wasm-client", target_arch = "wasm32"))]
+#[cfg(all(feature = "wasm_client", target_arch = "wasm32"))]
 pub use super::wasm::WasmClient as NativeClient;
